7d48b8d5006750b78fe10dd411cb80172b03211b
Shaikh Sabir <89734545+Sabirshaikh44@users.noreply.github.com>
2023-07-18 12:06:12 +0000

    Project Init

---
+++ main.js
@@ -0,0 +1,23 @@
+
+const inputField = document.getElementById("location");
+let address = "";
+ inputField.addEventListener('keyup',(e)=>{
+        address = e.target.value;   
+ })
+const getCord = async () =>{
+    if(address){
+        const res = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=AIzaSyDa9YpMUOQ4lyTonNUVSB14P7FN3J-BcQA`)
+        const data = await res.json()
+        console.log(data?.results[0]?.geometry?.location)
+        Swal.fire({
+            icon : 'success',
+            title : 'Cordinates fetched SuccessFully!',
+            text : `${address} is Located at Cordinates lat : ${data?.results[0]?.geometry?.location.lat} and long : ${data?.results[0]?.geometry?.location.lng}`,
+        })
+    }else{
+        Swal.fire({ icon: 'info',
+        title: 'Oops...',
+        text: 'Pls enter something or check if its valid address!'})
+    }
+}
+  
